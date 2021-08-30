5f14237dd5084c642736b1effb455a7dc5b14c42
wukashi <wukashisudol@gmail.com>
2021-08-30 18:40:32 +0200

    pierwszy commit

---
+++ src/main/java/pl/coderslab/AppInitializer.java
@@ -0,0 +1,17 @@
+package pl.coderslab;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class AppInitializer extends
+        AbstractAnnotationConfigDispatcherServletInitializer {
+    @Override
+    protected Class<?>[] getRootConfigClasses() { return null; }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class[]{AppConfig.class};  }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[]{"/"};   }
+}
---
+++ src/main/java/pl/coderslab/MockBookService.java
@@ -0,0 +1,5 @@
+package pl.coderslab;
+
+public class MockBookService {
+    //List<Book> list;
+}
